50d1199d9995c1c643bcaf68bca72042f5017b36
baturalpylmz <batur38907@gmail.com>
2025-12-04 00:54:41 +0300

    Remove step to copy index.html to 404.html in GitHub Actions workflow and switch to createHashRouter in router configuration

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,8 @@
 import MainLayout from '@/layouts/MainLayout';
 import Home from '@/pages/Home';
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: '/',
         element: <MainLayout />,
